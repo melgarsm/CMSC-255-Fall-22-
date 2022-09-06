d6f15d21328b72f71fb458c2d34052a28087e759
melgarsm <112972968+melgarsm@users.noreply.github.com>
2022-09-06 13:25:54 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/*******************
+*Hello.java
+*******************
+*Stefany Melgar 
+*9/06/2022
+* CMSC 255
+*********************/
+
+public class Hello {
+	public static void main (String[] args) {
+
+		System.out.println("<Stefany>");
+		System.out.println("<Hello.java>"); 
+		System.out.println("<Getting java to work properly>"); 
+		System.out.println("<Version 9/6/22>"); 
+		System.out.println("<CMSC 255>"); 
+		System.out.println(""); 
+
+
+	}
+}
